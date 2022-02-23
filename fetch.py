baa16da47cb3b126fb666ea22b9efe7b5c0eac65
Fenil Babariya <75973998+fenilbabariya@users.noreply.github.com>
2022-02-23 13:59:04 +0530

    Add files via upload

--- fetch.py
+++ fetch.py
@@ -10,5 +10,13 @@ try:
     for details in rows:
         print(details)
 
+    # Select data based on actorm name
+    print("Fetch movie based on actor name")
+    cursor = conn.execute("select * from Movies where actor='Aamir Khan'")
+    # Fetch all method to fetch data from cursor
+    rows = cursor.fetchall()
+    for details in rows:
+        print(details)
+
 except:
     print("Error in Fetch")
